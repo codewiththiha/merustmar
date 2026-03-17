c44e07996b97cb13121d35198e43bbf08f47a375
codewiththiha <kt007441@gmail.com>
2026-03-17 21:36:49 +0630

    update environment

--- src/environment.rs
+++ src/environment.rs
@@ -5,23 +5,39 @@ use crate::object::Object;
 #[derive(Debug, Clone, PartialEq)]
 pub struct Environment {
     store: HashMap<String, Object>,
+    outer: Option<Box<Environment>>,
+}
+
+impl Default for Environment {
+    fn default() -> Self {
+        Self::new()
+    }
 }
 
 impl Environment {
     pub fn new() -> Self {
         Environment {
             store: HashMap::new(),
+            outer: None,
+        }
+    }
+
+    pub fn new_enclosed(outer: Environment) -> Self {
+        Environment {
+            store: HashMap::new(),
+            outer: Some(Box::new(outer)),
         }
     }
 
     pub fn get(&self, name: &str) -> Option<Object> {
-        // We clone the object so we don't have lifetime issues returning references
-        self.store.get(name).cloned()
+        self.store
+            .get(name)
+            .cloned()
+            .or_else(|| self.outer.as_ref().and_then(|outer| outer.get(name)))
     }
 
     pub fn set(&mut self, name: String, val: Object) {
         // since this return value is rarely use already
-        self.store.insert(name, val.clone());
+        self.store.insert(name, val);
     }
 }
-
